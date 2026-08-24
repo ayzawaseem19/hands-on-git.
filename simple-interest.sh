413d9acdd20b6716122e31050fe748eb6713a465
ayzawaseem19 <ayzawaseem19@gmail.com>
2026-08-25 00:47:38 +0500

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter Principal amount:"
+read principal
+echo "Enter Rate of interest:"
+read rate
+echo "Enter Time (years):"
+read time
+
+interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+echo "Simple Interest = $interest"
